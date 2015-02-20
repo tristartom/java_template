8ea1df9023f347d9c79fa68417b3f185fa74f514
Yuzhe Tang <tristartom@gmail.com>
2015-02-19 20:04:02 -0500

    Update debug.sh

--- tt_sh/debug.sh
+++ tt_sh/debug.sh
@@ -7,5 +7,16 @@ echo " include live stacktrace."
 javaproc_pid=3278
 jstack -l $javaproc_pid
 
+jdb -classpath . $mainclass $argument
+# inside jdb, use commands:
+## stop in class.method
+## stop at class:linenumber
+## run
+## step #step into the method
+## next
+## where #stack frames in current thread
+## where all # ... in all threads
+## print var
+
 file_stdout_redirected=/tmp/stdout_redirected_tt
 ant debug 1>$file_stdout_redirected
